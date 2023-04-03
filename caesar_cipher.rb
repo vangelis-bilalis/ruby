da02c2bd47b2df2cf0f40507fa1723557b9cdbc1
vangelis-bilalis <vangelisbilalis@proton.me>
2023-04-03 22:24:24 +0300

    Almost optimized.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,30 +1,33 @@
 def caesar_cipher (str, shift)
   arr = str.split("")
 
+  shift = shift % 26
+
   arr = arr.map do |el|
     if (el.ord >= 97 && el.ord <= 122)
       if (el.ord + shift <= 122)
         el = (el.ord + shift).chr
       else
-        if (shift <= 26)
           el = (el.ord + shift - 26).chr
-        else
-          el = (el.ord + shift - 26 * (shift / 26)).chr
-        end
       end
     elsif (el.ord >= 65 && el.ord <= 90)
       if (el.ord + shift <= 90)
         el = (el.ord + shift).chr
       else
-        if (shift <= 26)
           el = (el.ord + shift - 26).chr
-        else
-          el = (el.ord + shift - 26 * (shift / 26)).chr
-        end
       end
+    else
+    	el
     end
   end
-  arr.join(" ")
+  
+  arr.join
 end
 
-puts caesar_cipher("abc de fgh ijk.", 1)
+puts "Enter the text that you want to encode: "
+str = gets.chomp
+
+puts "Enter the desired shift: "
+shift = gets.chomp.to_i
+
+puts caesar_cipher(str, shift)
