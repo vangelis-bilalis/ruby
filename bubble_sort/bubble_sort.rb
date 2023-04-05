d4187a3fdb38666e95918b984c8b93d8ec71eaa5
vangelis-bilalis <vangelisbilalis@proton.me>
2023-04-05 15:57:08 +0300

    bubble_sort stops sooner when it can.

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,16 +1,31 @@
 def bubble_sort(arr)
   j = arr.length - 2
 
+  #debug1
+  loops = 0
+
   while j >= 0
+    flag = false
+    
+    #debug1
+    loops += 1
+
     for i in 0..j
       if arr[i] > arr[i+1]
         temp = arr[i]
         arr[i] = arr[i+1]
         arr[i+1] = temp
+
+        flag = true
       end
     end
+    break if flag == false
+
     j -= 1
   end
+
+  #debug1
+  puts "number of loops = #{loops}"
 
   arr
 end
@@ -20,13 +35,7 @@ puts "Enter integers to be sorted, and enter 0 when you're done: "
 input = gets.chomp
 input = input.include?(".") ? input.to_f : input.to_i
 while input != 0
-  puts "current input:"
-  puts input
-
   arr.push(input)
-
-  puts "current array"
-  puts arr
 
   input = gets.chomp
   input = input.include?(".") ? input.to_f : input.to_i
